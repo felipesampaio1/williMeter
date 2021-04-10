581e30fab0e5e33aaeeff197b61284321d1cab88
Felipe Sampaio <fspreto@gmail.com>
2021-04-10 10:04:11 -0300

    Primeiro commit teste.

---
+++ williMeter/src/main/java/br/com/willi/williMeter/WilliMeterApplication.java
@@ -0,0 +1,13 @@
+package br.com.willi.williMeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilliMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilliMeterApplication.class, args);
+	}
+
+}
---
+++ williMeter/src/test/java/br/com/willi/williMeter/WilliMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.willi.williMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilliMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
