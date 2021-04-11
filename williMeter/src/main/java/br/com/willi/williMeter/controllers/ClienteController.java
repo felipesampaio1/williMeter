b92b3b27da6410abafac7c016b183d86153977bf
Felipe Sampaio <fspreto@gmail.com>
2021-04-10 22:53:00 -0300

    CRUD Cliente

--- williMeter/src/main/java/br/com/willi/williMeter/controllers/ClienteController.java
+++ williMeter/src/main/java/br/com/willi/williMeter/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.willi.williMeter.models.Cliente;
 import br.com.willi.williMeter.repository.ClienteRepository;
@@ -24,4 +25,12 @@ public class ClienteController {
 		cr.save(cliente);
 		return "redirect:/cadastrarCliente ";
 	}	
+	
+	@RequestMapping("/clientes")
+	public ModelAndView listaClientes() {
+		ModelAndView mv = new ModelAndView("cliente/listaClientes");
+		Iterable<Cliente> clientes = cr.findAll();
+		mv.addObject("clientes", clientes);
+		return mv;
+	}
 }
